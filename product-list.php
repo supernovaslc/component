82b22c356eab14c1020f8df6540577a0a27f45f0
Component <quang06121997@gmail.com>
2019-10-10 10:50:11 +0700

    Update product-list.php

--- product-list.php
+++ product-list.php
@@ -17,30 +17,60 @@ $products = array(
 		"star" => "image/star.png",
 		"votes" => 9,
 		"origin" => "Ha Noi"
+	],
+	[
+		"link" => "#",
+		"imagePro" => "image/mac.jpg",
+		"namePro" => "Macbook Pro",
+
+		"proInfo" => 'Macbook Pro 13 Touch Bar i5 1.4GHz/8G/128GB (2019)',
+		"linkInfo" => "#",
+
+		"currPrice" => "₫9,990,000",
+		"oldPrice" => "đ19,990,000",
+		"disPrice" => "30%",
+		"icon" => "image/free.png",
+
+		"star" => "image/star.png",
+		"votes" => 9,
+		"origin" => "Ha Noi"
+	],
+	[
+		"link" => "#",
+		"imagePro" => "image/mac.jpg",
+		"namePro" => "Macbook Pro",
+
+		"proInfo" => 'Macbook Pro 13 Touch Bar i5 1.4GHz/8G/128GB (2019)',
+		"linkInfo" => "#",
+
+		"currPrice" => "₫9,990,000",
+		"oldPrice" => "đ19,990,000",
+		"disPrice" => "30%",
+		"icon" => "image/free.png",
+
+		"star" => "image/star.png",
+		"votes" => 9,
+		"origin" => "Ha Noi"
 	]
 );
 
 function picture($link, $img, $alt){
-	echo '<div class="column">
-	<a href="'.$link.'">
-    	<img class="images" 
-    		src="'.$img.'" 
-    		alt="'.$alt.'" 
-    	/> 
-    </a> ';
-  	echo '</div>';
+	$html ='<a href="'.$link.'">
+		<img class="card-img-top" src="'.$img.'" alt="'.$alt.'" /> 
+	</a> 
+	';
+	return $html;
 }
 
 function productInfo($linkInfo, $proInfo){
-	echo '<div class="info">
-		<a href="'.$linkInfo.'">
-	    	'.$proInfo.'
-	    </a> ';
-  	echo '</div>';
+	$html ='<div class="info">
+		<a href="'.$linkInfo.'"> '.$proInfo.' </a> 
+	</div>';
+  	return $html;
 }
 
 function price($currPrice, $oldPrice, $disPrice, $icon){
-	echo '<div>
+	$html ='
 		<div class="price"><span>'.$currPrice.'</span></div>
 		<div class="price"> 
 			<span><del>'.$oldPrice.'</del></span>
@@ -48,11 +78,11 @@ function price($currPrice, $oldPrice, $disPrice, $icon){
 			<i class="images"><img src="'.$icon.'" /></i>
 		</div>
     ';
-  	echo '</div>';
+  	return $html;
 }
 
 function vote($star, $vote, $origin){
-	echo '<div>
+	$html ='
 		<div class="vote"> 
 			<i class="star"><img src="'.$star.'" /></i>
 			<i class="star"><img src="'.$star.'" /></i>
@@ -63,25 +93,46 @@ function vote($star, $vote, $origin){
 		</div>
 		<span class="origin">'.$origin.'</span>
     ';
-  	echo '</div>';
+  	return $html;
 }
 
 function product($link, $info, $curr, $old, $disc, $icons, $stars, $votes, $origins){
-	echo '<div class="column">
+	$html ='<div class="card-body">
 		'.productInfo($linkInfo=$link, $proInfo=$info).'
 		'.price($currPrice=$curr, $oldPrice=$old, $disPrice=$disc, $icon=$icons).'
 		'.vote($star=$stars, $vote=$votes, $origin=$origins).'
-    ';
-  	echo '</div>';
+	</div>';
+  	return $html;
 }
+
+// $html='<div class="card-deck">';
+// foreach($products as $key => $product){
+// 	$html .= '
+// 	<div class="card">
+// 		'.picture($product['link'], $product['imagePro'], $product['namePro']).'
+				
+// 		<div class="card-body">
+// 			'.product($product['linkInfo'], $product['proInfo'], $product['currPrice'], $product['oldPrice'], $product['disPrice'], $product['icon'], $product['star'], $product['votes'], $product['origin']).'
+// 		</div>
+// 	</div>';
+// }
+// $html .= '</div>';
 
 function App($products){
+	$html='<div class="card-deck">';
 	foreach ($products as $key => $product) {
-		picture($product['link'], $product['imagePro'], $product['namePro']);
-		product($product['linkInfo'], $product['proInfo'], $product['currPrice'], $product['oldPrice'], $product['disPrice'], $product['icon'], $product['star'], $product['votes'], $product['origin'],);
+		$html .= '<div class="card">
+			'.picture($product['link'], $product['imagePro'], $product['namePro']).'
+			
+			'.product($product['linkInfo'], $product['proInfo'], $product['currPrice'], $product['oldPrice'], $product['disPrice'], $product['icon'], $product['star'], $product['votes'], $product['origin']).'
+		</div>';
 	}
+	$html .='</div>';
+	return $html;
 }
 
+// echo $html;
+echo App($products);
 ?>
 <!DOCTYPE html>
 <html>
@@ -97,7 +148,7 @@ function App($products){
 <style type="text/css">
 	.card{
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-        max-width: 400px;
+        max-width: 300px;
         margin: auto;
         /* float: center; */
         text-align: left;
@@ -155,8 +206,6 @@ function App($products){
     }
 
 </style>
-	<div class="card">
-		<?php App($products); ?>
-	</div>
+
 </body>
 </html>
